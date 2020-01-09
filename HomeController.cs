bae13a2e885efabae2d1f555a99913ce1e5da73a
OgnjenR16 <59656067+OgnjenR16@users.noreply.github.com>
2020-01-09 13:04:57 +0100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PredIspit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Pocetna()
+        {
+            return View();
+        }
+
+        public ActionResult Planine()
+        {
+            ViewBag.Message = "Pogledajte najlepse od onoga cime Srbija raspolaze";
+
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            ViewBag.Message = "Уколико имате питање или замерку можете нас контактирати";
+
+            return View();
+        }
+    }
+}
